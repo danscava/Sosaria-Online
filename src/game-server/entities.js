dd08b3f87f86c8f57e29f82381fdd24b8db2cb5a
Norman B. Lancaster <qbradq@gmail.com>
2016-02-22 06:43:52 -0600

    Added game server framework

---
+++ src/game-server/entities.js
@@ -0,0 +1,4 @@
+"use static";
+
+var factory = require("../lib/constructor-factory");
+module.exports = new factory("./src/game-server/entities");
---
+++ src/game-server/server.js
@@ -0,0 +1,34 @@
+"use strict";
+
+require("../lib/extensions");
+
+var packetServer = require("../packet-server/server.js"),
+    cfg = require("../lib/config"),
+    log = require("../lib/log"),
+    store = require("../lib/store"),
+    SubscriberFactory = require("../lib/subscriber-factory"),
+    handlers = new SubscriberFactory("./src/game-server/handlers");
+
+log.init(cfg.logPath);
+store.init(cfg.dbPath);
+var server = new packetServer(cfg.ipv4, cfg.port);
+
+function atExit(err) {
+    if(err)
+        log.error("Error terminated process|" + err.stack);
+    if(atExit.done)
+        return;
+    atExit.done = true;
+    server.stop(() => {
+        store.close();
+    });
+}
+atExit.done = false;
+
+process.on("exit", atExit.bind(null));
+process.on("SIGINT", atExit.bind(null));
+process.on("uncaughtException", atExit.bind(null));
+
+handlers.subscribe(server);
+
+server.start();
